c574f83a33ddcaa661c4f6c71b3bcf741fb51b16
Felipe Lima <fearlimasi@gmail.com>
2021-11-08 17:09:22 -0300

    Commit inicial

---
+++ src/main/groovy/br/com/autorizai/api/ApiApplication.groovy
@@ -0,0 +1,13 @@
+package br.com.autorizai.api
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ApiApplication, args)
+	}
+
+}
---
+++ src/test/groovy/br/com/autorizai/api/ApiApplicationTests.groovy
@@ -0,0 +1,13 @@
+package br.com.autorizai.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
